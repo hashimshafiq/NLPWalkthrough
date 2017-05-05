7a3cdfa7b39cfc18575bcfdd6b39a8da0ac574cb
Muhammad Hashim Shafiq <hashimshafiq0@gmail.com>
2017-05-05 00:48:37 -0700

    Update Chapter1.py

--- Chapter1.py
+++ Chapter1.py
@@ -1,5 +1,38 @@
+from nltk.book import *  
 
-#Question 1
 print("Try using the Python interpreter as a calculator, and typing expressions like 12 / (4 + 1).")
+print(12/(4+1))
+
+
+print("Given an alphabet of 26 letters, there are 26 to the power 10, or 26 ** 10, ten-letter strings we can form. That works out to 141167095653376. How many hundred-letter strings are possible?")
+print(26**100)
+
+print("The Python multiplication operation can be applied to lists. What happens when you type ['Monty', 'Python'] * 20, or 3 * sent1?")
+sent1 = ['Monty','Python']
+print(sent1*20)
+print(3*sent1)
+
+print("How many words are there in text2? How many distinct words are there?")
+print(len(text2))
+print(len(set(text2)))
+
+print("Compare the lexical diversity scores for humor and romance fiction in 1.1. Which genre is more lexically diverse?")
+#Moby Dick by Herman Melville genre adventure 1851
+#Sense and Sensibility by Jane Austen genre romance 1811 text2
+#Monty Python and the Holy Grail genre comedy text6
+#The Man Who Was Thursday by G . K . Chesterton genre thriller 1908
+print(100 * len(set(text2))/len(text2))
+print(100 * len(set(text6))/len(text6))
+
+print("Produce a dispersion plot of the four main protagonists in Sense and Sensibility: Elinor, Marianne, Edward, and Willoughby. What can you observe about the different roles played by the males and females in this novel? Can you identify the couples?")
+text2.dispersion_plot(["Elinor", "Marianne", "Edward", "Willoughby"])
+
+print("Find the collocations in text5.")
+text5.collocations()
+
+
+print("Consider the following Python expression: len(set(text4)). State the purpose of this expression. Describe the two steps involved in performing this computation.")
+print("It gets the vocabulary for the text4 and then return its len")
+
 
 
